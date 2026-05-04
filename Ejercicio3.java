86c811b6330ecf2423f69c063353d9935dc97fe3
Samuel <samruiz168@gmail.com>
2026-05-03 23:04:05 -0600

    Ejercicio3 commit

---
+++ Ejercicio3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa el primer número: ");
+        int a = sc.nextInt();
+
+        System.out.print("Ingresa el segundo número: ");
+        int b = sc.nextInt();
+
+        System.out.print("Ingresa el tercer número: ");
+        int c = sc.nextInt();
+
+        int suma = a + b + c;
+        double promedio = (double) suma / 3;
+        int producto = a * b * c;
+
+        int menor = a;
+        int mayor = a;
+
+        if (b < menor) menor = b;
+        if (c < menor) menor = c;
+        if (b > mayor) mayor = b;
+        if (c > mayor) mayor = c;
+
+        System.out.println("Suma: " + suma);
+        System.out.printf("Promedio: %.2f%n", promedio);
+        System.out.println("Producto: " + producto);
+        System.out.println("Menor: " + menor);
+        System.out.println("Mayor: " + mayor);
+
+        sc.close();
+    }
+}
